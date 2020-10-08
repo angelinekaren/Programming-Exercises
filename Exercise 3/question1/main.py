7c9a7963ebef764833c61c4a5a13a831540de5c6
angelinekaren <71621472+angelinekaren@users.noreply.github.com>
2020-10-08 14:59:44 +0700

    Update main.py

--- Exercise 3/question1/main.py
+++ Exercise 3/question1/main.py
@@ -4,6 +4,7 @@ def convert_to_days():
     minutes = int(input("Enter number of minutes: "))
     seconds = int(input("Enter number of seconds: "))
     print("The number of days is: ", round(get_days(), 4))
+    return get_days()
 def get_days():
     number_of_days = float(((seconds/3600)+(minutes/60)+hours)/24)
     return number_of_days
